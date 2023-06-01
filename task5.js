c4df0364498df31179f70f4fdad34c51e678c74e
nikhilverma2020 <93897102+nikhilverma2020@users.noreply.github.com>
2023-06-01 13:13:49 +0530

    Update task5.js

--- task5.js
+++ task5.js
@@ -1,7 +1,4 @@
-// <!-- Submitted by: -->
-// <!-- Khushi Ojha (20BHI10053) -->
-// <!-- VIT BHOPAL UNIVERSITY -->
-// <!-- Task 5 -->
+
 function reverseNumber(num) {
     var reversedString = num.toString().split('').reverse().join('');
     var reversedNumber = parseInt(reversedString, 10);

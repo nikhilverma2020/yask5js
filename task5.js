dbc9c5c5f04ffba543b3777ba0d7352de1dee87e
nikhilverma2020 <93897102+nikhilverma2020@users.noreply.github.com>
2023-06-01 13:14:44 +0530

    Delete task5.js

--- task5.js
+++
@@ -1,9 +0,0 @@
-
-function reverseNumber(num) {
-    var reversedString = num.toString().split('').reverse().join('');
-    var reversedNumber = parseInt(reversedString, 10);
-    return reversedNumber;
-  }
-var x = 32243;
-var reversedX = reverseNumber(x);
-console.log(reversedX)  
